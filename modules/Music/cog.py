8584c68c035c7d3f939dc656eaa27f0ed0c7ea7b
master142009 <82803040+master142009@users.noreply.github.com>
2022-03-14 20:10:07 +0530

    Update cog.py

--- modules/Music/cog.py
+++ modules/Music/cog.py
@@ -10,31 +10,7 @@ class Music(commands.Cog, name="Music"):
     def __init__(self, bot: commands.Bot):
         self._bot = bot 
 
-class MakeLinkBtn(nextcord.ui.view):
-    def __init__(self, Link:str):
-        super().__init__()
-        self.add_item(nextcord.ui.Button(Label="Join Game!", url=f"{Link}"))        
-
-    @commands.group(imvoke_without_commands = True)
-    async def play(ctx):
-        return
-
-    @play.command()
-    async def sketch(ctx, channel: nextcord.TextChannel = None):
-        if channel == None:
-            return await ctx.send("please specifly a channel type to join/create a game")
-
-        try:
-            invite_link = await channel.create_activity_invite(activities.Activity.sketch)
-        except nextcord.HTTPException:
-            return ctx.send("please mention a voice channel to join/create a game")
-        em = nextcord.Embed(title="Sketch Game", description=f"{ctx.author.mention} has been created a game in {channel.mention}")
-        em.add_field(name="How to Play?", description="its like skribble.io but in a vc... someone draws something and everyone else has to guess what it is.")
-        em.set_thumbnail(url="https://support.discord.com/hc/article_attachments/4503731144471/Discord_SketchHeads_Lobby.png")
-
-        await ctx.send(embed=em)
-
-
+    
 
 
 def setup(bot):
