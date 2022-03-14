c3e4ad9213c5986bb42085a135aa70a40af162e5
master142009 <82803040+master142009@users.noreply.github.com>
2022-03-14 21:23:41 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,11 +3,7 @@ from dotenv import load_dotenv
 import nextcord
 from nextcord.ext import commands ,tasks, activities
 from itertools import cycle
-
-class MakeLinkBtn(nextcord.ui.view):
-    def __init__(self, Link:str):
-        super().__init__()
-        self.add_item(nextcord.ui.Button(Label="Join Game!", url=f"{Link}"))
+from nextcord.ui import Button, View
 
 client = commands.Bot(command_prefix="m!")
 
@@ -26,34 +22,19 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+    hi = Button(label="Invite me", url="https://discord.com/api/oauth2/authorize?client_id=888675856497643561&permissions=8&scope=bot")
+    myview = View(timeout=180)
+    myview.add_item(hi)
     if client.user.mentioned_in(message):
-        Embed = nextcord.Embed(title="The NextGen Bot", description="Hey! if your new, my prefix is `m!`\nuse `m!help` for all my commands. \n[Invite me](https://discord.com/api/oauth2/authorize?client_id=888675856497643561&permissions=8&scope=bot)", colour=3066993)
+        Embed = nextcord.Embed(title="The NextGen Bot", description="Hey! if your new, my prefix is `m!`\nuse `m!help` for all my commands.", colour=3066993)
         Embed.set_author(name=f"{client.user.name}",
                     icon_url=f"{client.user.avatar.url}")
         Embed.set_footer(
             text=f"Mentioned by {message.author.name}", icon_url=f"{message.author.avatar.url}") 
-        await message.channel.send(embed=Embed)
+        await message.channel.send(embed=Embed, view=myview)
 
     await client.process_commands(message)
 
-@client.group(imvoke_without_commands = True)
-async def play(ctx):
-    return
-
-@play.command()
-async def sketch(ctx, channel: nextcord.TextChannel = None):
-    if channel == None:
-        return await ctx.send("please specifly a channel type to join/create a game")
-
-    try:
-        invite_link = await channel.create_activity_invite(activities.Activity.sketch)
-    except nextcord.HTTPException:
-        return ctx.send("please mention a voice channel to join/create a game")
-    em = nextcord.Embed(title="Sketch Game", description=f"{ctx.author.mention} has been created a game in {channel.mention}")
-    em.add_field(name="How to Play?", description="its like skribble.io but in a vc... someone draws something and everyone else has to guess what it is.")
-    em.set_thumbnail(url="https://support.discord.com/hc/article_attachments/4503731144471/Discord_SketchHeads_Lobby.png")
-
-    await ctx.send(embed=em, view=MakeLinkBtn(invite_link))
 
     # load all cogs
 for folder in os.listdir("modules"):
