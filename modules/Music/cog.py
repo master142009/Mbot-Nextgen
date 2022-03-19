69c806b0e71708c8f267638fe942cbaed57ddd94
master142009 <82803040+master142009@users.noreply.github.com>
2022-03-19 16:18:40 +0530

    Update cog.py

--- modules/Music/cog.py
+++ modules/Music/cog.py
@@ -1,5 +1,25 @@
+import asyncio
+
+import async_timeout
+import wavelink
 from nextcord import ClientException
 from nextcord.ext import commands
+from wavelink import (
+    LavalinkException,
+    LoadTrackError,
+    SoundCloudTrack,
+    YouTubeMusicTrack,
+    YouTubePlaylist,
+    YouTubeTrack,
+)
+from wavelink.ext import spotify
+from wavelink.ext.spotify import SpotifyTrack
+
+from ._classes import Provider
+from .checks import voice_channel_player, voice_connected
+from .errors import MustBeSameChannel
+from .paginator import Paginator
+from .player import DisPlayer
 
 class music(commands.Cog, name="music"):
     """Recives Music commands"""
