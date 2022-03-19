13df6fade49cb43aeb289d8a25f9576d3930eba0
master142009 <82803040+master142009@users.noreply.github.com>
2022-03-19 16:05:11 +0530

    Delete modules/Music directory

--- modules/Music/_classes.py
+++
@@ -1,6 +0,0 @@
-from typing import Union
-
-from wavelink import SoundCloudTrack, YouTubeMusicTrack, YouTubeTrack
-from wavelink.ext.spotify import SpotifyTrack
-
-Provider = Union[YouTubeTrack, YouTubeMusicTrack, SoundCloudTrack, SpotifyTrack]
