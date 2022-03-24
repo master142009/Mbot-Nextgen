2944f2b713ff970c7460ddb4325cf852366afdd4
master142009 <82803040+master142009@users.noreply.github.com>
2022-03-24 19:20:48 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,6 +8,8 @@ from nextcord.ui import Button, View
 import datetime
 import lavalink
 import json    
+import aiosqlite
+import random
 
 def get_prefix(client, message):
     with open('prefixes.json', 'r') as f:
@@ -39,6 +41,48 @@ async def change_status_text():
 async def on_ready():
     change_status_text.start()
     print(f"{client.user.name} has connected to Discord.")
+    setattr(client, 'db', aiosqlite.connect("level.db"))
+    await aiosqlite.sleep(3)
+    async with client.db.cursor() as cursor:
+        await cursor.execute("CREATE TABLE IF NOT EXISTS levelS (level INTEGER, xp INTEGER, user INTEGER, guild INTEGER)")
+
+@client.event
+async def on_message(message):
+    if message.author.client:
+        return
+    author = message.author
+    guild = message.guild
+    async with client.db.cursor() as cursor:
+        await cursor.execute("SELECT xp FROM levels WHERE user = ? AND guild = ?", {author.id, guild.id})
+        xp = await cursor.fetchone()
+        await cursor.execute("SELECT level FROM levels WHERE user = ? AND guild = ?", {author.id, guild.id})
+        level = await cursor.fetchone()
+
+        if not xp or not level:
+            await cursor.execute("INSERT INTO levels (level, xp, user, guild) VALUES (?, ?, ?, ?)", {0, 0, author.id, guild.id})
+            await client.commit()
+
+        try:
+            xp = xp[0]
+            level = level[0]
+        except TypeError:
+            xp = 0
+            level = 0
+
+        if level > 5:
+            xp = random.randint(1, 3)
+            await cursor.execute("UPDATE levels SET xp = ? WHERE user = ? AND guild = ?", {xp, author.id, guild.id})
+        else:
+            rand = random.randint(1, (level//4))
+            if rand == 1:
+                xp = random.randint(1, 3)
+                await cursor.execute("UPDATE levels SET xp = ? WHERE user = ? AND guild = ?", {xp, author.id, guild.id})
+        if xp >= 100:
+            level += 1
+            await cursor.execute("UPDATE levels SET level = ? WHERE user = ? AND guild = ?", {xp, author.id, guild.id})
+            await cursor.execute("UPDATE levels SET xp = ? WHERE user = ? AND guild = ?", {xp, author.id, guild.id})
+            await message.channel.send(f"{author.mention} has leveled up to level **{level}**!")
+    await client.db.commit()                             
 
 @client.event
 async def on_guild_join(guild):
