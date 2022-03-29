a4f08d65c0717d5ae4e7ca2a2e2d272153f64c5d
master142009 <82803040+master142009@users.noreply.github.com>
2022-03-29 20:58:43 +0530

    Delete modules/Jishaku directory

--- modules/Jishaku/cog.py
+++
@@ -1,9 +0,0 @@
-from nextcord.ext import commands
-
-from jishaku.cog import STANDARD_FEATURES, OPTIONAL_FEATURES
-
-class CustomDebugCog(*OPTIONAL_FEATURES, *STANDARD_FEATURES):
-    pass
-
-def setup(bot: commands.Bot):
-    bot.add_cog(CustomDebugCog(bot=bot))
