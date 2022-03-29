3bd58cf751d90aae467ac0cf3bba29424a7f947d
master142009 <82803040+master142009@users.noreply.github.com>
2022-03-29 15:05:01 +0530

    Update cog.py

--- modules/random/cog.py
+++ modules/random/cog.py
@@ -173,14 +173,14 @@ class Random(commands.Cog, name="Random"):
 
             background.rectangle((30, 220), width=650, height=40, color="#90EE90", radius=20)
             background.bar((30, 220), max_width=650, height=40, percentage=user_data["percentage"], color="#66ff00", radius=20,)
-            background.text((200, 40), user_data["name"], font=poppins, color="#0000FF")
+            background.text((200, 40), user_data["name"], font=poppins, color="#ADD8E6")
 
             background.rectangle((200, 100), width=350, height=2, fill="#90EE90")
             background.text(
                 (200, 130),
                 f"Level - {user_data['level']}  |  {user_data['xp']} / {user_data['next_level_up']}",
                 font = poppins_small,
-                color = "#0000FF",
+                color = "#ADD8E6",
             )
 
             file = nextcord.File(fp=background.image_bytes, filename="levelcard.png")
